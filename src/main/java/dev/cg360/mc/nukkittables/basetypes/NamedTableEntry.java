2e0cb0832d9f41442c37f8368890f3de27a213fb
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-02 11:45:15 +0100

    Removed setters from table entries.

--- src/main/java/dev/cg360/mc/nukkittables/basetypes/NamedTableEntry.java
+++ src/main/java/dev/cg360/mc/nukkittables/basetypes/NamedTableEntry.java
@@ -3,6 +3,5 @@ package dev.cg360.mc.nukkittables.basetypes;
 public interface NamedTableEntry {
 
     String getName();
-    void setName();
 
 }
--- src/main/java/dev/cg360/mc/nukkittables/basetypes/ParentTableEntry.java
+++ src/main/java/dev/cg360/mc/nukkittables/basetypes/ParentTableEntry.java
@@ -3,6 +3,5 @@ package dev.cg360.mc.nukkittables.basetypes;
 public interface ParentTableEntry {
 
     TableEntry[] getChildren();
-    void setChildren();
 
 }
