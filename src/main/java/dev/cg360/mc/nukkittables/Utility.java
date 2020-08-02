f7daaa116c32d44690f4539a55878cd8a1a830d7
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-02 20:31:19 +0100

    Added Range generation to Utility

--- src/main/java/dev/cg360/mc/nukkittables/Utility.java
+++ src/main/java/dev/cg360/mc/nukkittables/Utility.java
@@ -1,10 +1,16 @@
 package dev.cg360.mc.nukkittables;
 
 import cn.nukkit.item.Item;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import dev.cg360.mc.nukkittables.math.FloatRange;
+import dev.cg360.mc.nukkittables.math.IntegerRange;
 import dev.cg360.mc.nukkittables.types.TableCondition;
 import dev.cg360.mc.nukkittables.types.TableFunction;
 import dev.cg360.mc.nukkittables.context.TableRollContext;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class Utility {
@@ -22,6 +28,38 @@ public class Utility {
         Item returnable = item;
         for(TableFunction func: functions) returnable = func.applyFunction(returnable, context);
         return returnable;
+    }
+
+    public static Optional<IntegerRange> getIntegerRangeFromData(JsonObject data){
+        JsonElement minimumObject = data.get("min");
+        JsonElement maximumObject = data.get("max");
+        if(minimumObject instanceof JsonPrimitive && maximumObject instanceof JsonPrimitive){
+            JsonPrimitive minimum = (JsonPrimitive) minimumObject;
+            JsonPrimitive maximum = (JsonPrimitive) maximumObject;
+            if(minimum.isNumber() && maximum.isNumber()){
+                int min = minimum.getAsInt();
+                int max = maximum.getAsInt();
+
+                return Optional.of(new IntegerRange(min, max));
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<FloatRange> getFloatRangeFromData(JsonObject data){
+        JsonElement minimumObject = data.get("min");
+        JsonElement maximumObject = data.get("max");
+        if(minimumObject instanceof JsonPrimitive && maximumObject instanceof JsonPrimitive){
+            JsonPrimitive minimum = (JsonPrimitive) minimumObject;
+            JsonPrimitive maximum = (JsonPrimitive) maximumObject;
+            if(minimum.isNumber() && maximum.isNumber()){
+                float min = minimum.getAsFloat();
+                float max = maximum.getAsFloat();
+
+                return Optional.of(new FloatRange(min, max));
+            }
+        }
+        return Optional.empty();
     }
 
     public static int getRandomIntBetweenInclusiveBounds(int min, int max){
