3cd7adf3217a53e14b18b2132e4c25bfffd5f6ec
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-02 19:35:58 +0100

    Added ConditionID

--- src/main/java/dev/cg360/mc/nukkittables/basetypes/TableCondition.java
+++ src/main/java/dev/cg360/mc/nukkittables/basetypes/TableCondition.java
@@ -6,8 +6,12 @@ import dev.cg360.mc.nukkittables.context.TableRollContext;
 public abstract class TableCondition {
 
     protected String condition;
-
     protected JsonObject data;
+
+    public TableCondition(String conditionID, JsonObject data){
+        this.condition = conditionID;
+        this.data = data;
+    }
 
     public abstract boolean isConditionPassed(TableRollContext context);
 
