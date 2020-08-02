8a58223e700650fc7bd3a6fe9ba2216b19ccf32f
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-02 11:28:42 +0100

    Add mass apply function.

--- src/main/java/dev/cg360/mc/nukkittables/Utility.java
+++ src/main/java/dev/cg360/mc/nukkittables/Utility.java
@@ -1,6 +1,8 @@
 package dev.cg360.mc.nukkittables;
 
+import cn.nukkit.item.Item;
 import dev.cg360.mc.nukkittables.basetypes.TableCondition;
+import dev.cg360.mc.nukkittables.basetypes.TableFunction;
 import dev.cg360.mc.nukkittables.context.TableContext;
 
 public class Utility {
@@ -12,6 +14,12 @@ public class Utility {
             }
         }
         return true;
+    }
+
+    public static Item applyFunctions(TableFunction[] functions, Item item, TableContext context){
+        Item returnable = item;
+        for(TableFunction func: functions) returnable = func.applyFunction(returnable, context);
+        return returnable;
     }
 
 }
