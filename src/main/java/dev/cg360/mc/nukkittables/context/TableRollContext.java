fcaf7b12fe801a276188957d14cff90f66bc3e7e
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-02 20:41:37 +0100

    Added Location (origin) to common TableRollContext

--- src/main/java/dev/cg360/mc/nukkittables/context/TableRollContext.java
+++ src/main/java/dev/cg360/mc/nukkittables/context/TableRollContext.java
@@ -1,4 +1,16 @@
 package dev.cg360.mc.nukkittables.context;
 
+import cn.nukkit.level.Location;
+
 public class TableRollContext {
+
+    protected Location origin;
+
+    public TableRollContext(Location origin){
+        this.origin = origin;
+    }
+
+    public Location getOrigin() {
+        return origin;
+    }
 }
