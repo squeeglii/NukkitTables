55c9fd4b20a3da603faef23293a01ca3ef15ab06
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-03 18:37:47 +0100

    Added getters for Registry maps

--- src/main/java/dev/cg360/mc/nukkittables/LootTableRegistry.java
+++ src/main/java/dev/cg360/mc/nukkittables/LootTableRegistry.java
@@ -7,9 +7,12 @@ import dev.cg360.mc.nukkittables.functions.FunctionExecutorSetCount;
 import dev.cg360.mc.nukkittables.types.LootTable;
 import dev.cg360.mc.nukkittables.executors.TableConditionExecutor;
 import dev.cg360.mc.nukkittables.executors.TableFunctionExecutor;
+import dev.cg360.mc.nukkittables.types.TableCondition;
 import dev.cg360.mc.nukkittables.types.entry.TableEntry;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 public class LootTableRegistry {
 
@@ -55,7 +58,17 @@ public class LootTableRegistry {
         entryTypes.put(entryType.getType().toLowerCase(), entryType.getClass());
     }
 
-    public static LootTableRegistry getRegistry(){
+    public Optional<TableConditionExecutor> getConditionExecutor(String id){ return Optional.ofNullable(conditionExecutors.get(id.toLowerCase())); }
+    public Optional<TableFunctionExecutor> getFunctionExecutor(String id){ return Optional.ofNullable(functionExecutors.get(id.toLowerCase())); }
+    public Optional<Class<? extends TableEntry>> getEntryTypeClass(String id){ return Optional.ofNullable(entryTypes.get(id.toLowerCase())); }
+    public Optional<LootTable> getLootTable(String id){ return Optional.ofNullable(lootTables.get(id.toLowerCase())); }
+
+    public ArrayList<String> getConditionExecutors() { return new ArrayList<>(conditionExecutors.keySet()); }
+    public ArrayList<String> getFunctionExecutors() { return new ArrayList<>(functionExecutors.keySet()); }
+    public ArrayList<String> getEntryTypes() { return new ArrayList<>(entryTypes.keySet()); }
+    public ArrayList<String> getLootTables() { return new ArrayList<>(lootTables.keySet()); }
+
+    public static LootTableRegistry get(){
         return INSTANCE;
     }
 
