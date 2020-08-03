62d7509ec644ad3a2863251bc9eb9d3d2455cca9
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-03 13:05:09 +0100

    Added the beginning of loading for Functions and Conditions

--- src/main/java/dev/cg360/mc/nukkittables/types/TableCondition.java
+++ src/main/java/dev/cg360/mc/nukkittables/types/TableCondition.java
@@ -1,7 +1,13 @@
 package dev.cg360.mc.nukkittables.types;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import dev.cg360.mc.nukkittables.context.TableRollContext;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public abstract class TableCondition {
 
@@ -14,6 +20,20 @@ public abstract class TableCondition {
     }
 
     public abstract boolean isConditionPassed(TableRollContext context);
+
+    public static Optional<TableCondition> loadConditionFromJsonObject(JsonObject object){
+        JsonElement conditionElement = object.get("condition");
+
+        if(conditionElement instanceof JsonPrimitive){
+            JsonPrimitive conditionPrimitive = (JsonPrimitive) conditionElement;
+            if(conditionPrimitive.isString()){
+                String name = conditionPrimitive.getAsString();
+                // Data is JsonObject
+                //TODO: get condition from registry
+            }
+        }
+        return Optional.empty();
+    }
 
     public String getConditionType() { return condition; }
     public JsonObject getData() {
--- src/main/java/dev/cg360/mc/nukkittables/types/TableFunction.java
+++ src/main/java/dev/cg360/mc/nukkittables/types/TableFunction.java
@@ -1,9 +1,15 @@
 package dev.cg360.mc.nukkittables.types;
 
 import cn.nukkit.item.Item;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import dev.cg360.mc.nukkittables.Utility;
 import dev.cg360.mc.nukkittables.context.TableRollContext;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public abstract class TableFunction {
 
@@ -21,6 +27,32 @@ public abstract class TableFunction {
 
     public final Item applyFunction(Item item, TableRollContext context){
         return Utility.compileConditions(conditions, context) ? applyFunctionToItem(item) : item;
+    }
+
+    public static Optional<TableFunction> loadFromJsonObject(JsonObject object){
+        JsonElement functionElement = object.get("function");
+        JsonElement conditionsElement = object.get("conditions");
+
+        if(functionElement instanceof JsonPrimitive){
+            JsonPrimitive functionPrimitive = (JsonPrimitive) functionElement;
+
+            if(functionPrimitive.isString()){
+                String name = functionPrimitive.getAsString();
+                ArrayList<TableCondition> cs = new ArrayList<>();
+
+                if(conditionsElement instanceof JsonArray){
+                    for(JsonElement c: (JsonArray) conditionsElement){
+                        if(c instanceof JsonObject) {
+                            TableCondition.loadConditionFromJsonObject((JsonObject) c).ifPresent(cs::add);
+                        }
+                    }
+                }
+
+                // Data is JsonObject
+                //TODO: get condition from registry
+            }
+        }
+        return Optional.empty();
     }
 
     protected abstract Item applyFunctionToItem(Item item);
