ad0dbd6a8eb59c6f425ba832799690dc3ed1f3f9
CG360 <CloudGamer360@users.noreply.github.com>
2020-08-03 15:07:37 +0100

    Remove JsonObject parameter from TimeCheck

--- src/main/java/dev/cg360/mc/nukkittables/conditions/ConditionExecutorTimeCheck.java
+++ src/main/java/dev/cg360/mc/nukkittables/conditions/ConditionExecutorTimeCheck.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 
 public class ConditionExecutorTimeCheck extends TableConditionExecutor {
 
-    public ConditionExecutorTimeCheck(JsonObject data) {
+    public ConditionExecutorTimeCheck() {
         super("minecraft:time_check");
     }
 
