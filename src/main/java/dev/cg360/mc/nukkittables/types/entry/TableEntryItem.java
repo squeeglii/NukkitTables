a445ff65a862560901c1e48d8124c312ce87728d
CG360 <CloudGamer360@users.noreply.github.com>
2020-11-03 20:25:45 +0000

    Final restructure for consistency

--- src/main/java/dev/cg360/mc/nukkittables/types/entry/TableEntryItem.java
+++ src/main/java/dev/cg360/mc/nukkittables/types/entry/TableEntryItem.java
@@ -64,25 +64,22 @@ public class TableEntryItem extends TableEntry implements NamedTableEntry {
             JsonPrimitive namePrimitive = (JsonPrimitive) nameElement;
 
             if(!(namePrimitive.isNumber() || namePrimitive.isString())) return false;
-            this.name = namePrimitive.getAsString();
 
+            ArrayList<TableFunction> funcs = new ArrayList<>();
 
             if(functionsElement instanceof JsonArray) {
                 JsonArray functionsArray = (JsonArray) functionsElement;
 
-                ArrayList<TableFunction> funcs = new ArrayList<>();
                 for(JsonElement f: functionsArray){
                     if(f.isJsonObject()){
                         JsonObject func = (JsonObject) f;
                         TableFunction.loadFromJsonObject(func).ifPresent(funcs::add);
                     }
                 }
-
-                this.functions = funcs.toArray(new TableFunction[0]);
-            } else {
-                this.functions = new TableFunction[0];
             }
 
+            this.name = namePrimitive.getAsString();
+            this.functions = funcs.toArray(new TableFunction[0]);
 
             return true;
         }
